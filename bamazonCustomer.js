d7c67bdce3c304b2d158e49bb15d390583e7fc2e
Emery I <ihegaranyau@gmail.com>
2019-04-27 20:20:19 -0400

    created a function to prompt users

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,5 +17,60 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+    displayItems();
+    searchProduct();
 });
+
+function displayItems() {
+    var query = "SELECT * FROM bamazon_db.products";
+    connection.query(query, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id);
+            console.log(res[i].product_name);
+            console.log(res[i].price);
+        }
+
+    });
+
+}
+
+function searchProduct() {
+    inquirer
+        .prompt({
+            name: "item_id",
+            type: "input",
+            message: "What is your product's item id?"
+        })
+        .then(function(answer) {
+            console.log(answer);
+            var query = "SELECT * FROM bamazon_db.products WHERE item_id=" + answer.item_id;
+            connection.query(query, function(err, res) {
+                console.log(res);
+                // for (var i = 0; i < res.length; i++) {
+                //   console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+                // }
+                // runSearch();
+            });
+        });
+    productQuantity();
+}
+
+function productQuantity() {
+    inquirer
+        .prompt({
+            name: "stock_quantity",
+            type: "input",
+            message: "how many units of the product they would like to buy?"
+        })
+        .then(function(answer) {
+            console.log(answer);
+            // var query = "SELECT * FROM bamazon_db.products WHERE item_id=" + answer.item_id;
+            // connection.query(query, function(err, res) {
+            //     console.log(res);
+            //     // for (var i = 0; i < res.length; i++) {
+            //     //   console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+            //     // }
+            //     // runSearch();
+            // });
+        });
+}
